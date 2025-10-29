aa0820ce9d23cd67d5ac66c400def275c209d90b
0xRichardL <levanthanh.ptit@gmail.com>
2025-10-29 11:48:42 +0700

    try optimize substring

--- leetcode/longest-substring-without-repeating-characters/main.go
+++ leetcode/longest-substring-without-repeating-characters/main.go
@@ -2,16 +2,16 @@ package main
 
 import "fmt"
 
-func isRepeated(s string) bool {
+func isRepeated(s string, l, r int) bool {
 	if len(s) < 1 {
 		return false
 	}
-	m := make(map[rune]bool)
-	for _, r := range s {
-		if m[r] {
+	m := make(map[byte]bool)
+	for i := l; i < r; i++ {
+		if m[s[i]] {
 			return true
 		}
-		m[r] = true
+		m[s[i]] = true
 	}
 	return false
 }
@@ -25,7 +25,7 @@ func lengthOfLongestSubstring(s string) int {
 	for wsize <= len(s) {
 		i := 0
 		for (i + wsize) <= len(s) {
-			if !isRepeated(s[i : i+wsize]) {
+			if !isRepeated(s, i, i+wsize) {
 				max = wsize
 				break
 			}
