c9bd2a3c0782e43557e2a5ed16304bdf34b71f75
0xRichardL <levanthanh.ptit@gmail.com>
2025-10-30 17:26:57 +0700

    try more way

--- leetcode/median-of-two-sorted-arrays/main.go
+++ leetcode/median-of-two-sorted-arrays/main.go
@@ -4,26 +4,64 @@ import (
 	"fmt"
 )
 
-// 4ms | Beats 18.64%
+// // 4ms | Beats 18.64%
+// func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+// 	merged := make([]int, 0, len(nums1)+len(nums2))
+// 	i, j := 0, 0
+// 	for i < len(nums1) && j < len(nums2) {
+// 		if nums1[i] < nums2[j] {
+// 			merged = append(merged, nums1[i])
+// 			i++
+// 		} else {
+// 			merged = append(merged, nums2[j])
+// 			j++
+// 		}
+// 	}
+// 	if i < len(nums1) {
+// 		merged = append(merged, nums1[i:]...)
+// 	}
+// 	if j < len(nums2) {
+// 		merged = append(merged, nums2[j:]...)
+// 	}
+
+// 	mid := len(merged) / 2
+// 	if len(merged)%2 == 1 {
+// 		return float64(merged[mid])
+// 	}
+// 	return (float64(merged[mid]) + float64(merged[mid-1])) / 2
+// }
+
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	merged := make([]int, 0, len(nums1)+len(nums2))
-	i, j := 0, 0
-	for i < len(nums1) && j < len(nums2) {
-		if nums1[i] < nums2[j] {
-			merged = append(merged, nums1[i])
-			i++
-		} else {
-			merged = append(merged, nums2[j])
-			j++
+	l := len(nums1) / 2
+	r := len(nums2) / 2
+	// fmt.Println("l:", l)
+	// fmt.Println("r:", r)
+	if len(nums1) > 0 && len(nums2) > 0 {
+		for l != len(nums1)-1 && r != 0 {
+			// fmt.Println(1, nums1, nums2)
+			if nums1[l] > nums2[r] {
+				c := nums2[r]
+				nums2[r] = nums1[l]
+				nums1[l] = c
+			} else {
+				for nums1[l] <= nums2[r] && l < len(nums1)-1 {
+					l++
+				}
+				for nums1[l] < nums2[r] && r > 0 {
+					r--
+				}
+			}
+		}
+		// fmt.Println(nums1, nums2)
+		if nums1[l] > nums2[r] {
+			c := nums2[r]
+			nums2[r] = nums1[l]
+			nums1[l] = c
 		}
 	}
-	if i < len(nums1) {
-		merged = append(merged, nums1[i:]...)
-	}
-	if j < len(nums2) {
-		merged = append(merged, nums2[j:]...)
-	}
-
+	// fmt.Println(l, r)
+	// fmt.Println(nums1, nums2)
+	merged := append(nums1, nums2...)
 	mid := len(merged) / 2
 	if len(merged)%2 == 1 {
 		return float64(merged[mid])
@@ -32,11 +70,13 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 }
 
 func main() {
-	fmt.Println("[1,3] [2] \t\t2.0 =>", findMedianSortedArrays([]int{1, 3}, []int{2}))                             // 2.0
-	fmt.Println("[1,2] [3,4] \t\t2.5 =>", findMedianSortedArrays([]int{1, 2}, []int{3, 4}))                        // 2.5
-	fmt.Println("[0,0] [0,0] \t\t0.0 =>", findMedianSortedArrays([]int{0, 0}, []int{0, 0}))                        // 0.0
-	fmt.Println("[] [1] \t\t\t1.0 =>", findMedianSortedArrays([]int{}, []int{1}))                                  // 1.0
-	fmt.Println("[2] [] \t\t\t2.0 =>", findMedianSortedArrays([]int{2}, []int{}))                                  // 2.0
-	fmt.Println("[1] [2] \t\t1.5 =>", findMedianSortedArrays([]int{1}, []int{2}))                                  // 1.5
-	fmt.Println("[2,2,4,4] [2,2,2,4,4] \t2.0 =>", findMedianSortedArrays([]int{2, 2, 4, 4}, []int{2, 2, 2, 4, 4})) // 3.0
+	// fmt.Println("[1,3] [2] \t\t2.0 =>", findMedianSortedArrays([]int{1, 3}, []int{2}))                                              // 2.0
+	// fmt.Println("[1,2] [3,4] \t\t2.5 =>", findMedianSortedArrays([]int{1, 2}, []int{3, 4}))                                         // 2.5
+	// fmt.Println("[0,0] [0,0] \t\t0.0 =>", findMedianSortedArrays([]int{0, 0}, []int{0, 0}))                                         // 0.0
+	// fmt.Println("[] [1] \t\t\t1.0 =>", findMedianSortedArrays([]int{}, []int{1}))                                                   // 1.0
+	// fmt.Println("[2] [] \t\t\t2.0 =>", findMedianSortedArrays([]int{2}, []int{}))                                                   // 2.0
+	// fmt.Println("[1] [2] \t\t1.5 =>", findMedianSortedArrays([]int{1}, []int{2}))                                                   // 1.5
+	fmt.Println("[2,2,4,4] [2,2,4,4] \t3.0 =>", findMedianSortedArrays([]int{2, 2, 4, 4}, []int{2, 2, 4, 4})) // 3.0
+	// fmt.Println("[2,2,4,4] [2,2,2,4,4] \t2.0 =>", findMedianSortedArrays([]int{2, 2, 4, 4}, []int{2, 2, 2, 4, 4})) // 2.0
+	// fmt.Println("[0,0,0,0,0] [-1,0,0,0,0,0,1] \t 0.0=>", findMedianSortedArrays([]int{0, 0, 0, 0, 0}, []int{-1, 0, 0, 0, 0, 0, 1})) // 0.0
 }
