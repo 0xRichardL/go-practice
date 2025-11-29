e617e4b1d6032d3d913dd615f8b2558a3ad14453
Richard <thanhle@ancient8.gg>
2025-11-29 22:39:54 +0700

    basic channel usages & deadlock cases basic goroutine usage with wait group

--- channel/basic.go
+++ channel/basic.go
@@ -1,19 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func f() {
-	fmt.Println("B")
+// Single goroutine for sending and main goroutine receiving from a channel.
+func single() {
+	ch := make(chan string)
+
+	go func() {
+		// Send a message to the channel.
+		ch <- "Hello from goroutine"
+	}()
+	// Receive the message from the channel.
+	// This will block until a message is available.
+	msg := <-ch
+	fmt.Printf("Received from goroutine: \"%s\"\n", msg)
+	close(ch)
+	msg, ok := <-ch // Receiving from a closed channel returns the zero value.
+	fmt.Printf("Received from closed channel: \"%s\", ok: %t\n", msg, ok)
+}
+
+func multiple() {
+	ch := make(chan string)
+
+	// Start multiple sender goroutines.
+	for i := 0; i < 3; i++ {
+		go func(id int) {
+			now := time.Now().Nanosecond()
+			msg := fmt.Sprintf("Hello from goroutine %d at %dns", id, now)
+			ch <- msg
+			// Case 2: Safe close(ch) at last process
+			if i == 2 {
+				close(ch)
+			}
+		}(i)
+		// This is needed to avoid race condition when spawning close goroutines.
+		// Millis is safe thread.
+		time.Sleep(1 * time.Millisecond)
+		// These have a change to panic since the scheduled duration too small.
+		time.Sleep(1 * time.Microsecond)
+		time.Sleep(1 * time.Nanosecond)
+	}
+	// Case 1 (safe): Only read a expected messages.
+	for i := 0; i < 3; i++ {
+		msg := <-ch
+		fmt.Printf("Received: \"%s\"\n", msg)
+	}
+	// Case 2: Will read all message but deadlock at the end since no close(ch) from the last sender.
+	for msg := range ch {
+		fmt.Printf("Received: \"%s\"\n", msg)
+	}
 }
 
 func main() {
-	// Trigger Go to spawn go Routine, instantly.
-	// Create a new lightweight thread of execution and run this function independently.
-	// Schedule the f() call at near future.
-	go f()
-	// f() can be scheduled here.
-	fmt.Println("A")
-	// f() can be scheduled here.
+	single()
+	multiple()
 }
-
-// f() can be scheduled here (when process exit and NO f() called).
