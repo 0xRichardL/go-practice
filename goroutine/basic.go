4bd4f41ef6ac67a02fceaecda94c743b9dfb743a
0xRichardL <levanthanh.ptit@gmail.com>
2025-12-01 21:02:16 +0700

    add goroutines count

--- goroutine/basic.go
+++ goroutine/basic.go
@@ -1,12 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
 
 func f() {
 	fmt.Println("B")
 }
 
 func main() {
+	fmt.Println("Number of goroutines at start:", runtime.NumGoroutine())
 	// Trigger Go to spawn go Routine, instantly.
 	// Create a new lightweight thread of execution and run this function independently.
 	// Schedule the f() call at near future.
@@ -14,6 +18,7 @@ func main() {
 	// f() can be scheduled here.
 	fmt.Println("A")
 	// f() can be scheduled here.
+	fmt.Println("Number of goroutines:", runtime.NumGoroutine())
 }
 
 // f() can be scheduled here (when process exit and NO f() called).
