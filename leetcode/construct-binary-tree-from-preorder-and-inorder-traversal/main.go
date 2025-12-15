f892c13a83b64d316306c2113466c4f7454b93dc
0xRichardL <levanthanh.ptit@gmail.com>
2025-12-15 23:35:21 +0700

    divide and conquer solution

--- leetcode/construct-binary-tree-from-preorder-and-inorder-traversal/main.go
+++ leetcode/construct-binary-tree-from-preorder-and-inorder-traversal/main.go
@@ -6,13 +6,26 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+// 462ms Beats 5.05%
+// 78.52MB Beats 5.73%
 func buildTree(preorder []int, inorder []int) *TreeNode {
 	m := make(map[int]int)
 	for i, v := range inorder {
 		m[v] = i
 	}
-	nodes := make([]*TreeNode, 0)
+	subPreorder := make([]int, 0)
 	for _, v := range preorder {
-		nodes = append(nodes, &TreeNode{Val: v})
+		if _, ok := m[v]; ok {
+			subPreorder = append(subPreorder, v)
+		}
 	}
+	if len(subPreorder) == 0 {
+		return nil
+	}
+	root := &TreeNode{}
+	root.Val = subPreorder[0]
+	idx := m[root.Val]
+	root.Left = buildTree(subPreorder, inorder[:idx])
+	root.Right = buildTree(subPreorder, inorder[idx+1:])
+	return root
 }
